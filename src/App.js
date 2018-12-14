cb07571b6d61311d6a797b66de9aca62388c5d44
Munawar <munawar.jaffer@googlemail.com>
2018-12-14 09:36:02 +0000

    Creating a function in React

--- src/App.js
+++ src/App.js
@@ -3,19 +3,27 @@ import dcComics from './dc-comics.png';
 import marvelComics from './marvel-comics.jpg';
 import './App.css';
 
+function Home() {
+  return (
+    <div>
+      <h1>Welcome to the Heros app!!</h1>
+      <div className='container'>
+        <div className='column'>
+          <img src={dcComics} alt='DC comics Logo' />
+        </div>
+        <div className='column'>
+          <img src={marvelComics} alt='Marvel comics Logo' />
+        </div>
+      </div>
+    </div>)
+}
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Welcome to the Heros app!!</h1>
-        <div className='container'>
-          <div className='column'>
-            <img src={dcComics} alt='DC comics Logo' />
-          </div>
-          <div className='column'>
-            <img src={marvelComics} alt='Marvel comics Logo' />
-          </div>
-        </div>
+        <Home/>
+        <Home/>
       </div>
     );
   }
